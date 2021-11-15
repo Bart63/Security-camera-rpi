6d8fc2f9814bb3ed685ab24aecf52b1ba010b0d3
Bart63 <bartek.durys@gmail.com>
2021-11-15 07:50:19 +0100

    Form template

--- user_view.py
+++ user_view.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
+DEBUG = True
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    if request.method == 'GET':
+        return render_template('form.html')
+    else:
+        print(request.form)
+        return redirect(request.url)
 
 
 if __name__ == '__main__':
